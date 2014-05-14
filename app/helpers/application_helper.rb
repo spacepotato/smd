beca65dba18f8ec71be8122aa659a2e43f71c5f1
James McLaren <jamesmclaren555@gmail.com>
2014-05-14 12:49:30 +1000

    Navbar now working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,3 @@
 module ApplicationHelper
 
-def my_button_to name, options = {}, html_options = {} # or some variation
-  # eg. deal with options hash the way button_to deals with it here?
-  content_tag :button, html_options = nil do
-  	raw name
-  end
-end
-
-def include_javascript (file)
-    s = " <script type=\"text/javascript\">" + render(:file => file) + "</script>"
-    content_for(:head, raw(s))
-end
-
 end
