ceb3df3974c3908768d3ae88402314e2c350a104
James McLaren <jamesmclaren555@gmail.com>
2014-05-14 16:19:54 +1000

    Added additional fields to the user registration page and changed it so that a user logs in with the username and password (to change this back to email go into the devise.rb file under config and change the authentication key)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513060300) do
+ActiveRecord::Schema.define(version: 20140514044810) do
 
   create_table "clubs", force: true do |t|
     t.string   "name"
