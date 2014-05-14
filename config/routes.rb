4c48f3d8038375e340081e5f4299ed8b5adfe8ab
James McLaren <jamesmclaren555@gmail.com>
2014-05-14 13:39:18 +1000

    Navbar now has login/logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :clubs
 
-  devise_for :users
+  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
