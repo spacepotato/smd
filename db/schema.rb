e402b457c19be0b8bf8c585c4e800d8f9bd722b1
jordan121sa <jakean@student.unimelb.edu.au>
2014-05-16 14:14:09 +1000

    Heading for clubs, and avi's mum

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140514044810) do
+ActiveRecord::Schema.define(version: 20140516025655) do
 
   create_table "clubs", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140514044810) do
     t.integer  "registrationNumber"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "event_id"
   end
 
   create_table "events", force: true do |t|
