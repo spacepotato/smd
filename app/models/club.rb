40ed50753fc1f5ffbe8172c7a6c2e047f35075bb
James McMahon <whichoneiwonder@gmail.com>
2014-05-20 19:03:00 +1000

    running

---
+++ app/models/club.rb
@@ -0,0 +1,10 @@
+class Club < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+has_many :events
+belongs_to :club_admin
+
+end
