9da6cfbd2fe23f46bcb25109c732269e2fc332ab
James McLaren <jamesmclaren555@gmail.com>
2014-05-20 22:01:24 +1000

    Add search to Clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,9 +2,25 @@ class Club < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
-has_many :events
-belongs_to :club_admin
+  has_many :events
+  belongs_to :club_admin
+
+  def self.search(search)
+  	if search
+  		clubs = Array.new
+
+  		Club.all.each do |temp_club|
+  			if temp_club.name.include? search || temp_club.name == search
+  				clubs.push(temp_clubs)
+  			end
+  		end
+
+  		return clubs
+  	else
+  		return Club.all
+  	end
+  end
 
 end
