166ee9964ca7ec2ebbf1ca82362f266d94ae8470
James McMahon <whichoneiwonder@gmail.com>
2014-05-23 22:12:14 +1000

    stopped weird redirects in create club

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -65,8 +65,12 @@ class ClubsController < ApplicationController
     @club = Club.new(club_params)
     if !validate_club(@club.name)
       flash[:error] = "This is not a registered club"
+      redirect_to :back
+      return
     elsif does_club_exist?(@club.name)
         flash[:error] = "This is club already exists"
+        redirect_to :back
+        return
     else
     @club_admin = ClubAdmin.new({:user => current_user,:club => @club, :position => "El Presidente"})
     # @club_admin.club_id = @club.id
@@ -87,7 +91,8 @@ class ClubsController < ApplicationController
       end
     end
   end
-  redirect_to :back
+
+  return
   end
 
   # PATCH/PUT /clubs/1
