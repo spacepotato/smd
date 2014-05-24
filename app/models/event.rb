7b6ab66ec154d8e9de07802cd6848f11bf4224bf
Avi <avnishjain22@gmail.com>
2014-05-24 14:05:56 +1000

    CSS Stylings to Banner Image

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,7 +15,7 @@ has_attached_file :banner, :styles => {
       :thumb => "100x100#",
       :small  => "150x150#",
       :medium => "300x300#",
-      :banner => "500x200#"}
+      :banner => "900x200#"}
 
 has_attached_file :image, :styles => {
       :thumb => "100x100#",
